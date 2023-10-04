c3223a47858fed85bfa2aa6dcc3b972f26dda4ca
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-04 11:02:24 +0200

    insert whatwedo component & component card dinamic

---
+++ src/components/CardWeDo.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'CardWeDo',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        title: String,
+        desc: String,
+        path: String,
+    },
+    methods: {
+        getImagePath: function (img) {
+            return new URL(img, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col my-5">
+        <div class="card" style="width:18rem;">
+            <img :src="getImagePath(path)" alt="...">
+            <div class="card-body">
+                <h5 class="card-title my-4">{{ title }}</h5>
+
+                <p class="card-text">
+                    {{ desc }}
+                </p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -1,12 +1,45 @@
 <script>
+
+import CardWEDo from './CardWeDo.vue'
+
 export default {
-    name: 'WhatWeDo'
+    name: 'WhatWeDo',
+    data() {
+        return {
+            cards_we_do: [
+                {
+                    image: '../assets/img/Group-247.png',
+                    title: 'Data Analysis',
+                    description: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+                },
+                {
+                    image: '../assets/img/Group-567.png',
+                    title: 'SEO Optimization',
+                    description: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+                },
+                {
+                    image: '../assets/img/Group-538.png',
+                    title: 'Security Data',
+                    description: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+                },
+                {
+                    image: '../assets/img/Group-566.png',
+                    title: 'Branding Strategy',
+                    description: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+                },
+            ]
+        }
+    },
+    components: {
+        CardWEDo,
+    }
 }
 </script>
 
 <template>
     <section id="what_we_do">
         <div class="container">
+
             <div class="text-center">
                 <h6 class="mt-5">Our Services</h6>
                 <h2><span class="fw-bold">What </span>We Do</h2>
@@ -14,6 +47,18 @@ export default {
                     When, while the lovely valley teems with vapour around meand the
                 </p>
             </div>
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+
+                <CardWEDo v-for="card in cards_we_do" :path="card.image" :title="card.title" :desc="card.description">
+                </CardWEDo>
+
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-danger rounded-pill btn_attention my-5">View All Services</button>
+            </div>
+
 
         </div>
 
