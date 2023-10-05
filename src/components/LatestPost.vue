8426c66c1d68605a4004ca0ef1e0c2df3a41c05b
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-05 10:29:39 +0200

    complete LatestPost com with card andstyle

--- src/components/LatestPost.vue
+++ src/components/LatestPost.vue
@@ -20,21 +20,62 @@ export default {
 
                 <div class="col">
                     <div class="card text-bg-dark position-relative">
+
                         <img src="../assets/img/headway-537308-unsplash-1380x703.jpg" class="card-img"
                             alt="person working on pc">
-                        <div class="card-img-overlay position-absolute bottom-0">
-                            <p class="card-text"><small>April 21,2019 - by Paul</small></p>
-                            <h5 class="card-title">Amazon gets CCI nod to acquire 49% stake in</h5>
-                            <p class="card-text">When, while the lovely valley teams with vapor around me and the meridian
-                                sun ...</p>
+                        <div class="card-img-overlay">
+                            <div
+                                class="position-absolute bottom-0 pb-4 d-flex justify-content-center align-items-end gap-2">
+                                <div class="col-8">
+                                    <p class="card-text"><small>April 21,2019 - by Paul</small></p>
+                                    <h5 class="card-title">Amazon gets CCI nod to acquire 49% stake in</h5>
+                                    <p class="card-text">When, while the lovely valley teams with vapor around me and the
+                                        meridian
+                                        sun ...</p>
+                                </div>
+                                <div class="col-4 text-center">
+                                    <button class="btn_white px-5 py-2 rounded-pill">Read More</button>
+                                </div>
+                            </div>
 
                         </div>
+
+
                     </div>
                 </div>
 
                 <div class="row row-cols-2">
-                    <div class="col"></div>
-                    <div class="col"></div>
+
+                    <div class="col">
+
+                        <div class="card_only_text card h-100">
+                            <div class="card-body d-flex flex-column justify-content-between align-items-start">
+                                <p class="card-text"><small>April 21,2019 - by Paul</small></p>
+                                <h5 class="card-title">Clearing Corporations get SEVI nod to invest in overnight funds</h5>
+                                <p class="card-text">When, while the lovely valley teams with vapor around me, and the
+                                    meridian sun...
+                                </p>
+                                <button class="btn_white px-5 py-2 rounded-pill">Read More</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col">
+
+                        <div class="card_only_text card h-100">
+                            <div class="card-body d-flex flex-column justify-content-between align-items-start">
+                                <p class="card-text"><small>April 21,2019 - by Paul</small></p>
+                                <h5 class="card-title">Clearing Corporations get SEVI nod to invest in overnight funds</h5>
+                                <p class="card-text">When, while the lovely valley teams with vapor around me, and the
+                                    meridian sun...
+                                </p>
+                                <button class="btn_white px-5 py-2 rounded-pill">Read More</button>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
