165811700eb69a8c42e5d181e07486a936f04d30
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-05 16:47:43 +0200

    fix active link and image padding

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
     data() {
         return {
             state,
-            active: '',
+            active: 0,
         }
     },
     components: {
@@ -21,9 +21,9 @@ export default {
     <header id="app_header">
 
         <nav class="navbar navbar-expand-sm container">
-            <div class="container-fluid justify-content-between">
+            <div class="container-fluid justify-content-between  align-items-center">
                 <a class="navbar-brand" href="#">
-                    <img src="../assets/img/white-logo-2.png" alt="Logo">
+                    <img src="../assets/img/white-logo-2.png" class="p-0" alt="Logo">
                 </a>
                 <div class="" id="">
                     <div class="navbar-nav gap-4">
